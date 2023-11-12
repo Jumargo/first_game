47028c3ba73ff7b873bd88d670767b298be8ff38
Juanjo <martinezgonzalezjuanjose@gmail.com>
2023-11-12 20:47:52 +0100

    personaje se mueve

--- juego_chorra/src/Player.cpp
+++ juego_chorra/src/Player.cpp
@@ -15,13 +15,32 @@ void Player::initialize() {
 }
 
 void Player::handleInput() {
-    // Manejo de la entrada del jugador
-    // Este código se traslada de main.cpp a aquí
+    // Suponiendo que quieras mover al personaje con las teclas del cursor.
+    // Establece la velocidad horizontal y vertical a 0 si no se presiona ninguna tecla.
+    velocity.x = 0.0f;
+    velocity.y = 0.0f;
+
+    // Comprobar las entradas del usuario y ajustar la velocidad en consecuencia.
+    // Estos valores se pueden ajustar para cambiar la sensación del movimiento.
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+        velocity.x = -200.0f;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+        velocity.x = 200.0f;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+        velocity.y = -200.0f;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+        velocity.y = 200.0f;
+    }
 }
 
 void Player::update(sf::Time deltaTime) {
-    // Actualizar la lógica del jugador
-    // Incluirá el movimiento y la detección de colisiones
+    // Actualizar la posición del personaje.
+    // deltaTime.asSeconds() convierte el tiempo de juego al tiempo en segundos,
+    // lo cual es necesario para un movimiento suave y dependiente del tiempo.
+    sprite.move(velocity * deltaTime.asSeconds());
 }
 
 void Player::render(sf::RenderWindow& window) {
